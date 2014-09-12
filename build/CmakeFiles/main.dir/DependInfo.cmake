2075495baf3a305fd5537dc67a70376a7254761c
Sakip Alias ZizouMgs <zizoumgs@hotmail.com>
2014-09-12 18:26:53 +0700

    First Cmake

---
+++ build/CmakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/zizou/aman(v.3)/alarm.c" "/home/zizou/aman(v.3)/build/CMakeFiles/main.dir/alarm.c.o"
+  "/home/zizou/aman(v.3)/child.c" "/home/zizou/aman(v.3)/build/CMakeFiles/main.dir/child.c.o"
+  "/home/zizou/aman(v.3)/database_.c" "/home/zizou/aman(v.3)/build/CMakeFiles/main.dir/database_.c.o"
+  "/home/zizou/aman(v.3)/dbCreate.c" "/home/zizou/aman(v.3)/build/CMakeFiles/main.dir/dbCreate.c.o"
+  "/home/zizou/aman(v.3)/global.c" "/home/zizou/aman(v.3)/build/CMakeFiles/main.dir/global.c.o"
+  "/home/zizou/aman(v.3)/local.c" "/home/zizou/aman(v.3)/build/CMakeFiles/main.dir/local.c.o"
+  "/home/zizou/aman(v.3)/main.c" "/home/zizou/aman(v.3)/build/CMakeFiles/main.dir/main.c.o"
+  "/home/zizou/aman(v.3)/menu.c" "/home/zizou/aman(v.3)/build/CMakeFiles/main.dir/menu.c.o"
+  "/home/zizou/aman(v.3)/modif.c" "/home/zizou/aman(v.3)/build/CMakeFiles/main.dir/modif.c.o"
+  "/home/zizou/aman(v.3)/setting.c" "/home/zizou/aman(v.3)/build/CMakeFiles/main.dir/setting.c.o"
+  "/home/zizou/aman(v.3)/sound.c" "/home/zizou/aman(v.3)/build/CMakeFiles/main.dir/sound.c.o"
+  "/home/zizou/aman(v.3)/table.c" "/home/zizou/aman(v.3)/build/CMakeFiles/main.dir/table.c.o"
+  "/home/zizou/aman(v.3)/tool.c" "/home/zizou/aman(v.3)/build/CMakeFiles/main.dir/tool.c.o"
+  "/home/zizou/aman(v.3)/tray.c" "/home/zizou/aman(v.3)/build/CMakeFiles/main.dir/tray.c.o"
+  "/home/zizou/aman(v.3)/waktu.c" "/home/zizou/aman(v.3)/build/CMakeFiles/main.dir/waktu.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CmakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/main.dir/waktu.c.o"
+  "CMakeFiles/main.dir/modif.c.o"
+  "CMakeFiles/main.dir/tool.c.o"
+  "CMakeFiles/main.dir/child.c.o"
+  "CMakeFiles/main.dir/setting.c.o"
+  "CMakeFiles/main.dir/alarm.c.o"
+  "CMakeFiles/main.dir/main.c.o"
+  "CMakeFiles/main.dir/global.c.o"
+  "CMakeFiles/main.dir/sound.c.o"
+  "CMakeFiles/main.dir/database_.c.o"
+  "CMakeFiles/main.dir/tray.c.o"
+  "CMakeFiles/main.dir/table.c.o"
+  "CMakeFiles/main.dir/local.c.o"
+  "CMakeFiles/main.dir/dbCreate.c.o"
+  "CMakeFiles/main.dir/menu.c.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
